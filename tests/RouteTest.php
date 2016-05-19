e0a753f59df033d9c872880c8f767de9d3aec484
Ben Overmyer <manatrance@gmail.com>
2016-05-19 12:57:03 -0500

    Set up for CircleCI.

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class RouteTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testHomeRoute()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit( '/' )
+             ->see( 'Amtgard' );
     }
 }
